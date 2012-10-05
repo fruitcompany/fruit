f6bf3f589b163ed09d75a36e2e2a60ffcc7e4a40
Shawn Jones <mryallups@gmail.com>
2012-10-05 00:31:42 -0700

    Revert "test changes"
    This reverts commit 9da5d21e2ab160dd0cdf39e6d0e0a14078cdc73d.

--- Home/home.js
+++ Home/home.js
@@ -4,7 +4,7 @@ Ext.require([
     //'Ext.tab.Panel'
     '*'
 ]);
-//This is a test!!!!!!!!!!!
+
 Ext.onReady(function() {
 
 	Ext.create('Ext.Viewport', {
@@ -13,11 +13,6 @@ Ext.onReady(function() {
 		items: [{
 				id:'horz1',
 				region:'north',
-				//baseCls:'x-plain',
-				//split:true,
-				//width:150,
-				//minWidth: 100,
-				//maxWidth: 250,
 				layout: {
 					type:'hbox',
 					padding: 10
@@ -25,34 +20,34 @@ Ext.onReady(function() {
 				margins: '5 0 5 5',
 				items: [{
 					id: 'icon',
-					html: '<img src="images/fruit_cup_logo_white_text.jpg" width="304" height="228">',
+					html: '<img src="images/fruit_cup_logo_white_text.jpg" width="200" height="175">',
 					region: 'west',
 					layout: 'fit',
-					border: 0
+					border: 0,
+					margins: '5 0 5 5'
 				}, {
 					id: 'title1',
-					html: '<h1 class="x-panel-header">Page Title</h1>',
-					region: 'east',
+					html: '<img src="images/GPAS.jpg" width="600" height="300">',
+					region: 'center',
 					layout: 'fit',
-					border: 0
+					border: 0,
+					margins: '5 0 5 120'
 				}]
 			}, {
 				id:'horz2',
 				region:'center',
-				//baseCls:'x-plain',
-				//split:false,
-				//width:150,
-				//minWidth: 100,
-				//maxWidth: 250,
 				layout: {
 					type:'hbox',
+					pack: 'center',
 					padding: 10
 				},
 				margins: '5 0 5 5',
 				items: [{
 					id: 'login',
+					xtype: 'panel',
 					layout: {
 						type:'vbox',
+						pack: 'start',
 						padding: 10
 					},
 					region: 'west',
@@ -64,8 +59,8 @@ Ext.onReady(function() {
 					}, {
 						id: 'password',
 						xtype: 'textfield',
-						fieldLabel: 'Password',
-						allowBlank: false
+						inputType: 'password',
+						fieldLabel: 'Password'
 					},{
 						id: 'login_button',
 						xtype: 'button',
@@ -73,13 +68,63 @@ Ext.onReady(function() {
 						handler: function() {
 							alert('You clicked the button!');
 							console.log('Logging In...');
+							var uName = Ext.getCmp('username');
+							var pName = Ext.getCmp('password');
+							console.log(uName.getValue() + ", " + pName.getValue());
 						}
 					}],
 					border: 0
 				}, {
-					id: 'title',
+					id: 'divider',
+					html: '<img src="images/divider.jpg" width="10" height="300">',
+					region: 'center',
+					layout: 'fit',
+					border: 0,
+					margins: '5 25 5 25'
+				}, {
+					id: 'createAccount',			
+					layout: {
+						type:'vbox',
+						pack: 'center',
+						padding: 10
+					},
 					region: 'east',
-					layout: 'fit'
+					items: [{
+						id: 'createUsername',
+						xtype: 'textfield',
+						fieldLabel: 'Username',
+						allowBlank: false
+					}, {
+						id: 'createPassword',
+						xtype: 'textfield',
+						inputType: 'password',
+						fieldLabel: 'Password'
+					}, {
+						id: 'createEmail',
+						xtype: 'textfield',
+						inputType: 'email',
+						fieldLabel: 'Email'
+					}, {
+						id: 'createStudentID',
+						xtype: 'textfield',
+						inputType: 'number',
+						fieldLabel: 'Student ID'
+					},{
+						id: 'create_button',
+						xtype: 'button',
+						text: 'Create User',
+						handler: function() {
+							alert('You clicked the button!');
+							console.log('Creating User...');
+							var uName = Ext.getCmp('createUsername');
+							var pName = Ext.getCmp('createPassword');
+							var eName = Ext.getCmp('createEmail');
+							var sName = Ext.getCmp('createStudentID');
+							var str = escape(uName.getValue()) + "&" + escape(pName.getValue()) + "&" + escape(eName.getValue()) + "&" + escape(sName.getValue());
+							window.location="../MainPage/index.html?" + str;
+						}
+					}],
+					border: 0
 				}]
 			}],
 		renderTo: Ext.getBody()
