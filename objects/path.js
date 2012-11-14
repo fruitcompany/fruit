ca2c74d71fd0b6305555d48c8cc49d5ea5e7a50a
Shawn Jones <mryallups@gmail.com>
2012-11-14 08:45:37 -0800

    Help Page update

---
+++ objects/path.js
@@ -0,0 +1,5 @@
+Ext.define('Path', {
+    requires: 'Semester',
+    alias: 'path'
+//     store: [['c1','class1'],['c2','class2'],['c3','class3'],['c4','class4'],['c5','class5']]
+}
---
+++ objects/semester.js
@@ -0,0 +1,5 @@
+Ext.define('Semester', {
+    extend: 'Ext.ux.form.MultiSelect',
+    alias: 'semester'
+//     store: [['c1','class1'],['c2','class2'],['c3','class3'],['c4','class4'],['c5','class5']]
+}
