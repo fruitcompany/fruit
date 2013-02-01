ed916de0a31272974b4dd743544d33ebe84f18ac
Shawn Jones <mryallups@gmail.com>
2013-02-01 15:59:12 -0800

    Missing comma

--- objects/path.js
+++ objects/path.js
@@ -1,5 +1,6 @@
 Ext.define('Path', {
     requires: 'Semester',
-    alias: 'path'
+    alias: 'path',
+    
 //     store: [['c1','class1'],['c2','class2'],['c3','class3'],['c4','class4'],['c5','class5']]
 }
