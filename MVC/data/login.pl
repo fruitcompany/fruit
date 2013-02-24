c5323ceac87b367b2594a4721b25d620c6ae519a
Shawn Jones <mryallups@gmail.com>
2013-02-23 16:07:01 -0800

    sfadadgsgadsgdsa

--- MVC/data/login.pl
+++ MVC/data/login.pl
@@ -3,30 +3,24 @@
 use DBI;
 #use DBD::mysql;
 use CGI;
-#use JSON;
-#use Encode;
 
-#print "Content-type: application.json";
+
 my $cgi = CGI->new;
-#my $data = $cgi->all_parameters,
-#my $self = shift;
-#my $params = $self->{params};
-#my $data = $params->{data};
 
 # MYSQL CONFIG VARIABLES
-$database = "gpas";
-$user     = "gpas";
-$pw       = "r3dh0tSvr*";
-$host     = "localhost";
-#$host     = "jd-research.ecs.csun.edu";
-$dsn      = "dbi:mysql:$database:$host";
+#$database = "gpas";
+#$user     = "gpas";
+#$pw       = "r3dh0tSvr*";
+#$host     = "localhost";
+##$host     = "jd-research.ecs.csun.edu";
+#$dsn      = "dbi:mysql:$database:$host";
 
 
 for my $param ($q->param()) {
     my $safe_param = $q->escapeHTML($param);
-    say $param;
+    print $param;
     for my $value ($q->param($param)) {
-        say $value;
+        print $value;
     }
 }
 
@@ -35,12 +29,12 @@ for my $param ($q->param()) {
 #print $cgi->header(-type => "application/json", -charset => "utf-8");
 #print $cgi->param("username").", ".$cgi->param("password");
 
-sub runSql {
-    my($sql)    = @_;
-    my $dbh     = DBI->connect($dsn, $user, $pw) or die print"Unable to connect: $DBI::errstr\n";
-    my $handler = $dbh->prepare($sql) or croak("error: " . $dbh->errstr);
- 
-    warn($sql . "\n");
-    $handler->execute or croak("error: " . $dbh->errstr);
-    return $handler;
-}
+#sub runSql {
+#    my($sql)    = @_;
+#    my $dbh     = DBI->connect($dsn, $user, $pw) or die print"Unable to connect: $DBI::errstr\n";
+#    my $handler = $dbh->prepare($sql) or croak("error: " . $dbh->errstr);
+# 
+#    warn($sql . "\n");
+#    $handler->execute or croak("error: " . $dbh->errstr);
+#    return $handler;
+#}
