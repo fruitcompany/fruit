d1870cf32ffd2f79a7ddb9b02639cdf3caf96f25
Brandyn Thornton <brandynt@gmail.com>
2013-02-24 11:59:38 -0800

    Added Json Content-Type to the response

--- MVC/app/rest.php
+++ MVC/app/rest.php
@@ -1,5 +1,5 @@
 <?phprequire_once 'logger/error_log.php'; //enables printing log messages to logger/error_log.txt
-spl_autoload_register('apiAutoload');function apiAutoload($classname){    if (preg_match('/[a-zA-Z]+Controller$/', $classname)) {        include __DIR__ . '/controller/' . $classname . '.php';        return true;    } }
+spl_autoload_register('apiAutoload');function apiAutoload($classname){    if (preg_match('/[a-zA-Z]+Controller$/', $classname)) {        include __DIR__ . '/controller/' . $classname . '.php';        return true;    } }	header('Content-Type: application/json');
 	$request_method = strtolower($_GET['_verb']);  	$model_name = $_GET['_m'];	error_log( "model " . $model_name );
 	error_log( "type " . $request_method );	if(isset($_GET['id']))		error_log( "id " . $_GET['id'] );
 	// route the request to the right place	$controller_name = $model_name . 'Controller';if (class_exists($controller_name)) {	$controller = new $controller_name();	$action_name = strtolower($request_method) . 'Action';	
